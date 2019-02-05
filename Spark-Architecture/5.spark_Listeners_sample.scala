863246bb5b1b2753b084eff45cef3bdb8d87f06e
Jayvardhan Reddy Vanchireddy <33086172+Jayvardhan-Reddy@users.noreply.github.com>
2019-02-05 14:53:37 -0400

    Update 5.spark_Listeners_sample.scala

--- Spark-Architecture/5.spark_Listeners_sample.scala
+++ Spark-Architecture/5.spark_Listeners_sample.scala
@@ -1,8 +1,8 @@
-#To execute EventLoggingListener
+//To execute EventLoggingListener
 val df = spark.read.json("/user/spark/applicationHistory/application_1540458187951_38909")
 df.printSchema
 
 df.select("Event").groupBy("Event").count.show(20,false)
 
-#To execute StatsReportListener
+//To execute StatsReportListener
 spark.read.text("sampleTxt").count
